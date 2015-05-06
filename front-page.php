d3b8393b57e4b0fd6c12332238098b577b938b5f
7Mears <7mears@gmail.com>
2015-05-06 17:45:10 -0600

    Added cake. Updated font awesome. site variable border-size. template-parts.

--- front-page.php
+++ front-page.php
@@ -18,6 +18,8 @@ get_header(); ?>
 
     <button>This is a button</button>
 
+    <p><i class="fa fa-birthday-cake"></i> This is a cake</p>
+
     <h1>The quick brown fox jumps over the lazy dog</h1>
     <h2>The quick brown fox jumps over the lazy dog</h2>
     <h3>The quick brown fox jumps over the lazy dog</h3>
--- functions.php
+++ functions.php
@@ -82,7 +82,7 @@ function eyedea_scripts() {
 	// Google Fonts
 	wp_register_style('googleFonts', 'http://fonts.googleapis.com/css?family=Roboto:500,900,400italic,100,700italic,300,700,500italic,100italic,300italic,400|Roboto+Slab:400,100,300,700');
 	wp_enqueue_style( 'googleFonts');
-	wp_enqueue_style( 'font-awesome', '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css', array(), '4.0.3' );
+	wp_enqueue_style( 'font-awesome', '//netdna.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css', array(), '4.0.3' );
 
 	// Comments
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
