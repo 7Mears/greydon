958d8ac5f6e3bea6cbb5d421d9bcbe724a7d2b36
7Mears <7mears@gmail.com>
2015-06-08 10:47:29 -0600

    added woo commerce support

--- functions.php
+++ functions.php
@@ -139,3 +139,9 @@ remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 remove_action( 'wp_print_styles', 'print_emoji_styles' );
 remove_action( 'admin_print_styles', 'print_emoji_styles' );
+
+// Woo Commerce support
+// add_action( 'after_setup_theme', 'woocommerce_support' );
+// function woocommerce_support() {
+//     add_theme_support( 'woocommerce' );
+// }
---
+++ woocommerce.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* @package eyedea
+*/
+get_header(); ?>
+
+<div class="container">
+  <main id="main" class="site-main" role="main">
+
+    <?php woocommerce_content(); ?>
+
+  </main>
+  <!-- /main -->
+</div>
+<!-- /container -->
+
+<?php get_footer(); ?>
