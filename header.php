230a5ecb54b55bcec876c2db9c537a69f57d2660
7Mears <7mears@gmail.com>
2015-06-09 14:29:02 -0600

    updated navigation

--- header.php
+++ header.php
@@ -28,7 +28,7 @@
         <!-- /site-branding -->
 
         <nav id="site-navigation" class="main-navigation" role="navigation">
-          <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">Menu <i class="fa fa-bars"></i>
+          <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><span class="menu-lines"></span>
           </button>
           <?php wp_nav_menu( array( 'theme_location'=>'primary', 'menu_id' => 'primary-menu' ) ); ?>
         </nav>
